a829e11c148c4ba26188881f7fffc8546f4bae8e
RYC0208 <ryc0208@naver.com>
2024-08-28 00:46:11 +0900

    PokemonDetail 컴포넌트 삭제

--- Pokemon-app/src/pages/Detail.jsx
+++ Pokemon-app/src/pages/Detail.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import MOCK_DATA from "../Mock";
-import PokemonDetail from "../components/PokemonDetail";
 import styled, { keyframes } from "styled-components";
 
 const PkmDetail = () => {
@@ -10,20 +9,19 @@ const PkmDetail = () => {
 
   const searchParams = new URLSearchParams(location.search);
   const pokemonId = Number(searchParams.get("id"));
-  console.log("pokemonId의 타입=>", typeof pokemonId);
   const pokemon = MOCK_DATA.find((p) => p.id === pokemonId);
 
-  const goBack = () => {
+  const onGoBack = () => {
     navigate(`/dex`);
   };
-  const prevPokemon = () => {
+  const onPrevPokemon = () => {
     if (pokemonId-1 > 0) {
       navigate(`/detail?id=${pokemonId - 1}`);
     }else {
       navigate(`/detail?id=${151}`);
     }
   };
-  const nextPokemon = () => {
+  const onNextPokemon = () => {
     if (pokemonId+1 < 152) {
       navigate(`/detail?id=${pokemonId + 1}`);
     } else{
@@ -32,12 +30,30 @@ const PkmDetail = () => {
   };
   return (
     <DetailPage>
-      <PokemonDetail
-        pokemon={pokemon}
-        onGoBack={goBack}
-        onNextPokemon={nextPokemon}
-        onPrevPokemon={prevPokemon}
-      />
+      {!pokemon ? (
+        <NoPokemonMessage>포켓몬을 찾을 수 없습니다.</NoPokemonMessage>
+      ) : (
+        <DetailContainer>
+          <DescriptionBox>
+              <GoBackButton onClick={onGoBack}>고 백</GoBackButton>
+            <ImageDetails>
+              <ImageContainer>
+                <PokemonImg src={pokemon.img_url} alt={pokemon.korean_name} />
+              </ImageContainer>
+              <DetailsContainer>
+                <PokemonNumber>No. {pokemon.id}</PokemonNumber>
+                <PokemonName>{pokemon.korean_name}</PokemonName>
+                <PokemonType>속성: {pokemon.types.join(", ")}</PokemonType>
+                <PokemonDescription>{pokemon.description}</PokemonDescription>
+              </DetailsContainer>
+            </ImageDetails>
+            <ButtonContainer>
+              <NextAndPrevButton onClick={onPrevPokemon}>이전 포켓몬</NextAndPrevButton>
+              <NextAndPrevButton onClick={onNextPokemon}>다음 포켓몬</NextAndPrevButton>
+            </ButtonContainer>
+          </DescriptionBox>
+        </DetailContainer>
+      )}
     </DetailPage>
   );
 };
@@ -61,4 +77,115 @@ const DetailPage = styled.div`
   height: 100vh;
 
   animation: ${fadeIn} 0.2s ease-in-out;
+`;
+
+const DetailContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+`;
+const DescriptionBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #ffffff;
+  border: 1px solid gray;
+  border-radius: 10px;
+  padding: 20px;
+  width: 100%;
+  max-width: 750px;
+  min-height: 200px;
+`;
+const ImageDetails = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const ImageContainer = styled.div`
+  flex-shrink: 0;
+  width: 150px;
+  height: 150px;
+`;
+
+const PokemonImg = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+const DetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const PokemonNumber = styled.p`
+  font-size: 12px;
+  color: gray;
+  margin: 0;
+`;
+
+const PokemonName = styled.h2`
+  font-size: 24px;
+  margin: 10px 0;
+`;
+
+const PokemonType = styled.p`
+  font-size: 14px;
+  margin: 5px 0;
+`;
+
+const PokemonDescription = styled.p`
+  font-size: 12px;
+  margin: 10px 0;
+  width: 400px;
+`;
+
+const ButtonContainer = styled.div`
+  margin-top: 20px;
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+`;
+
+const NextAndPrevButton = styled.button`
+  color: white;
+  border: 1px solid gray;
+  background-color: rgb(115, 197, 252);
+  border-radius: 5px;
+  width: 100px;
+  height: 40px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #3d779e;
+  }
+
+  &:active {
+    border: 2px solid black;
+  }
+`;
+const GoBackButton = styled.button`
+  position: absolute;
+  height: 35px;
+  width: 70px;  
+  border: none;
+  background-color: #e9e9e9b3;
+  border-radius: 5px;
+  margin-left: 530px;
+  
+  cursor: pointer;
+
+  &:hover {
+    background-color: #c9c9c9;
+  }
+
+  &:active {
+    border: 2px solid black;
+  }
+`
+const NoPokemonMessage = styled.div`
+  font-size: 18px;
+  color: gray;
+  text-align: center;
+  margin-top: 20px;
 `;
